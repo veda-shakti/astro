fc03093bdcf0bea3345daabdcfe4675b67d0d6de
IvanGudyushkin <ivanin.gud@gmail.com>
2023-11-24 13:51:46 +0300

    site update
    Add library "react-facebook-pixel" and "react-yandex-metriika"
    Fixed code  facebookpixel and yandexmetrica

--- src/components/Included/Metrics/YandexMetrica.jsx
+++ src/components/Included/Metrics/YandexMetrica.jsx
@@ -1,39 +1,39 @@
-/* global ym */
-
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { YMInitializer } from 'react-yandex-metrika';
 
 export class YandexMetrica extends Component {
     componentDidMount() {
-        // Проверка, был ли скрипт уже добавлен
-        if (!document.querySelector(`script[src="https://mc.yandex.ru/metrika/tag.js"]`)) {
-            // Создание скрипта
-            const script = document.createElement("script");
-            script.type = "text/javascript";
-            script.async = true;
-            script.src = "https://mc.yandex.ru/metrika/tag.js";
+        // Инициализация Метрики
+        Object.defineProperty(window, 'ym', {
+            value: window.ym || function() {
+                (window.ym.a = window.ym.a || []).push(arguments);
+            },
+            writable: true,
+            configurable: true
+        });
 
-            // Добавление скрипта в DOM
-            document.head.appendChild(script);
+        window.ym.l = 1 * new Date();
 
-            // Инициализация Метрики после загрузки скрипта
-            script.onload = () => {
-                window.ym = window.ym || function() {
-                    (window.ym.a = window.ym.a || []).push(arguments);
-                };
-                window.ym.l = 1 * new Date();
-
-                ym(95661099, "init", {
-                    clickmap: true,
-                    trackLinks: true,
-                    accurateTrackBounce: true,
-                    webvisor: true
-                });
-            };
-        }
+        window.ym(95661099, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
     }
 
     render() {
         // Отображение контента или ничего, если не требуется визуальное представление
-        return null;
+        return (
+            <YMInitializer
+                accounts={[95661099]}ы
+                options={{
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                }}
+            />
+        );
     }
 }
