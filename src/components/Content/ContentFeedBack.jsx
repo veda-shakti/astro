2221b80a630aeec545a56b75a93f98b5341ecad3
Alexandr Bondarenko <69047343+AlexUner@users.noreply.github.com>
2023-11-28 20:33:33 +0300

    Update ContentFeedBack.jsx

--- src/components/Content/ContentFeedBack.jsx
+++ src/components/Content/ContentFeedBack.jsx
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import '../../Styles/contentFeedBack.css'
 import '../../Styles/content.css'
+
 const ContentFeedBack = (props) => {
     // const [reviews, setReviews] = useState([]);
 
